e72a1100e18c1939e61468adcdca6dc2cdfce760
Mengyuan Wu <wmy1011@gmail.com>
2013-12-09 02:40:10 +0800

    Restructure the whole repo and redesign the iconfont demo

---
+++ js/iconfont.js
@@ -0,0 +1,20 @@
+$(function() {
+
+	$("#font-size-slider").change(function(e) {
+		$("tr:first-child td").css("font-size", $(this).val() + "px");	
+	});
+	
+	$(".color-slider").change(function(e) {
+		$("tr:first-child td").css("color", "hsla(" + $("#color-slider-1").val() + ", " + $("#color-slider-2").val() + "%, "
+		 + $("#color-slider-3").val() + "%, 1)");	
+	});
+	
+	$(".shadow-slider").change(function(e) {	
+		$("tr:first-child td").css("text-shadow", $("#shadow-slider-1").val() + "px " + $("#shadow-slider-2").val() + "px "
+		 + $("#shadow-slider-3").val() + "px black");	 
+	});
+
+    $('#font-size-slider-reset').click(function(e) {
+        $('.tr:first-child td').css('font-size',"62px");
+    });
+});
