d8ac5de134a8ebd919cc367e83aafbfb92feb533
wileam <wmy1011@gmail.com>
2013-12-18 21:36:52 +0800

    add two blank page and modify iconfont page

--- js/iconfont.js
+++ js/iconfont.js
@@ -1,20 +1,19 @@
-$(function() {
+$(document).ready(function() {
+	$(function() {
 
-	$("#font-size-slider").change(function(e) {
-		$("tr:first-child td").css("font-size", $(this).val() + "px");	
-	});
-	
-	$(".color-slider").change(function(e) {
-		$("tr:first-child td").css("color", "hsla(" + $("#color-slider-1").val() + ", " + $("#color-slider-2").val() + "%, "
-		 + $("#color-slider-3").val() + "%, 1)");	
-	});
-	
-	$(".shadow-slider").change(function(e) {	
-		$("tr:first-child td").css("text-shadow", $("#shadow-slider-1").val() + "px " + $("#shadow-slider-2").val() + "px "
-		 + $("#shadow-slider-3").val() + "px black");	 
-	});
+		$("#font-size-slider").change(function(e) {
+			$("tr:first-child td").css("font-size", $(this).val() + "px");	
+		});
+		
+		$(".color-slider").change(function(e) {
+			$("tr:first-child td").css("color", "hsla(" + $("#color-slider-1").val() + ", " + $("#color-slider-2").val() + "%, "
+			 + $("#color-slider-3").val() + "%, 1)");	
+		});
+		
+		$(".shadow-slider").change(function(e) {	
+			$("tr:first-child td").css("text-shadow", $("#shadow-slider-1").val() + "px " + $("#shadow-slider-2").val() + "px "
+			 + $("#shadow-slider-3").val() + "px black");	 
+		});
 
-    $('#font-size-slider-reset').change(function() {
-        $('.tr:first-child td').css('font-size','62px');
-    });
+	});
 });
